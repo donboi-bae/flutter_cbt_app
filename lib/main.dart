7a2e347db1ec0892d87955b92b869d2d67c9df72
Donboi <79328322+donboi-bae@users.noreply.github.com>
2023-12-05 00:36:58 +0700

    integration auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cbt/presentation/pages/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter CBT'),
+      home: const OnboardingPage(),
     );
   }
 }
